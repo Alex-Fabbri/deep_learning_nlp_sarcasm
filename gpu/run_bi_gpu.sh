08d9763993426e67a86e3cba8d8486a3ece655ef
Alexander R Fabbri <arf2145@orca.cs.columbia.edu>
2017-02-28 10:19:56 -0800

    reorganize

--- gpu/run_bi_gpu.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-THEANO_FLAGS=mode=FAST_RUN,device=gpu1,floatX=float32 python bi-lstm_ucsc_gpu.py output/ 300 False False 16 >> logs/log_bi_$1
-
---
+++ run_bi_gpu.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+THEANO_FLAGS=mode=FAST_RUN,device=gpu1,floatX=float32 python bi-lstm_ucsc_gpu.py output/ 300 False False 16 >> ../logs/log_bi_$1
+
